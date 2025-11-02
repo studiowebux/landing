a170704e80356369732c941ce106fc1e1361678f
tommy gingras <tommy@studiowebux.com>
2025-11-02 12:56:37 -0500

    cleanup local tests, added info

--- src/portfolio.js
+++ src/portfolio.js
@@ -258,10 +258,19 @@
           createLinkButton("JSR", project.links.jsr, createJSRIcon()),
         );
       }
+      if (project.links.appstore) {
+        linksDiv.appendChild(
+          createLinkButton(
+            "App Store",
+            project.links.appstore,
+            createAppleIcon(),
+          ),
+        );
+      }
 
       // Support for any other custom links
       Object.keys(project.links).forEach((key) => {
-        if (!["github", "website", "npm", "jsr"].includes(key)) {
+        if (!["github", "website", "npm", "jsr", "appstore"].includes(key)) {
           linksDiv.appendChild(
             createLinkButton(
               key.charAt(0).toUpperCase() + key.slice(1),
@@ -347,6 +356,17 @@
     svg.setAttribute("fill", "currentColor");
     svg.innerHTML =
       '<path d="M3 3h18v18H3V3zm16 16V5H5v14h14zM7 7h10v2H7V7zm0 4h10v2H7v-2zm0 4h7v2H7v-2z"/>';
+    return svg;
+  }
+
+  function createAppleIcon() {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "20");
+    svg.setAttribute("height", "20");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("fill", "currentColor");
+    svg.innerHTML =
+      '<path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/>';
     return svg;
   }
 
